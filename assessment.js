eb1272d58cd1b8bfb007176dfde7096cef73e885
watahiki <12813157+watahiki606@users.noreply.github.com>
2017-12-31 17:00:57 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -69,3 +69,34 @@
         '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
         '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を押さえられる{userName}が皆から評価されています。'
     ];
+
+    /**
+    * 名前の文字列を渡すと診断結果を返す関数
+    * @param {string} userName ユーザーの名前
+    * @return {string} 診断結果
+    */
+    function assessment(userName) {
+        // 全文字のコード番号を取得してそれを足し合わせる
+        let sumOfcharCode = 0;
+        for (let i = 0; i < userName.length; i++) {
+            sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
+        }
+
+        // 文字のコード番号の合計を回答の数で割って添字の数値を求める
+        const index = sumOfcharCode % answers.length;
+        let result = answers[index];
+
+        result = result.replace(/{userName}/g, userName);
+        return result;
+    }
+
+    // テストコード
+    console.assert(
+        assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+        '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+    );
+    console.assert(
+        assessment('太郎') === assessment('太郎'),
+        '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+    );
+})();
